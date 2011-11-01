71b18b485678861b8925fb241df085a82800754a
Morten Johansen <morten@bzzt.no>
2011-11-01 11:38:51 +0100

    - Outlined aprsSymbol class with constants for naming symbols. - Fixed aprsAutoload (again)

--- classes/aprsAutoload.class.php
+++ classes/aprsAutoload.class.php
@@ -1,10 +1,11 @@
 <?php
 
-$__PHPMYAPRSCONF = parse_ini_file('config.ini', true);
-
-function __autoload($class_name) {
-	global $__PHPMYAPRSCONF;
-
-	require_once(sprintf('%s/%s.class.php', $__PHPMYAPRSCONF['local']['classpath'], $class_name));
+class aprsAutoload {
+	static function autoload($class_name) {
+		$filename = sprintf('%s/%s.class.php', aprsConfig::get('local', 'classpath'), $class_name);
+		if(file_exists($filename)) {
+			require_once($filename);
+		}
+	}
 }
 
---
+++ classes/aprsSymbol.class.php
@@ -0,0 +1,7 @@
+<?php
+
+class aprsSymbol {
+	const EMERGENCY = '\!';
+	const JOGGER	= '/[';
+}
+
