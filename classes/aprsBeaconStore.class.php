5ef9032109314764beffc154608a3cd94584cc1b
Morten Johansen <morten@bzzt.no>
2011-11-03 21:06:06 +0100

    aprsBeaconStorw now working, but still lacking time based filtering

--- classes/aprsBeaconStore.class.php
+++ classes/aprsBeaconStore.class.php
@@ -2,11 +2,12 @@
 
 // For now this class IS the interface definition :)
 
-class aprsBeaconStore() {
+class aprsBeaconStore {
 	// This class uses the specified folder to store files for all stored beacons, and filesystem timestamps to determine when things are due for beaconing.
 	var $path;
 	
 	function __construct($path) {
+		$this->path = $path;
 		if(file_exists($path)) {
 			if(!is_dir($path)) throw new Exception(sprintf('%s: Specified path %s is not a directory', __METHOD__, $path));
 		} else {
@@ -27,11 +28,25 @@ class aprsBeaconStore() {
 	}
 
 	public function getBeacon($beacon_id) {
-		return unserialize(file_get_contents($this->getFilenameForId($beacon_id)));
+		return $this->getBeaconFromFile($this->getFilenameForId($beacon_id));
+	}
+
+	public function getBeaconFromFile($file) {
+		return unserialize(file_get_contents($file));
 	}
 	
 	public function getBeacons($changed_after=null, $accessed_before=null) {
-	
+		$ret = array();
+		$dir = opendir($this->getPath());
+		while($f = readdir($dir)) {
+			if(!preg_match(sprintf('/^%s/', $this->getFilenamePrefix()), $f)) continue;
+			$file = sprintf('%s/%s', $this->getPath(), $f);
+			$stat = stat($file);
+			// TODO: Implement atime and mtime filtering here...
+			$ret[] = $this->getBeaconFromFile($file);
+		}
+		closedir($dir);
+		return $ret;
 	}
 
 	/** Private functions **/
